b92d480da16774938e8a6d546a9fc465cf2faf1d
Jude-raganit <juderaganit56@gmail.com>
2024-10-18 11:05:38 +0800

    Create Array activity.js

---
+++ Array activity.js
@@ -0,0 +1,9 @@
+let favSingers = ["Taylor swift", "Billie Eilish", "Justin Bieber"];
+ console.log(favSingers[0]);
+
+
+let favNumbers = [6, 15, 23];
+console.log(favNumbers);
+
+let mixedArr = ["string", ["otherarray"], 123, true];
+console.log(mixedArr);
